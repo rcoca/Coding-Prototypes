8a051c6c563145771f745db500c1a090999f7703
Razvan Coca <razvan.coca@capgemini.com>
2024-01-30 17:43:05 +0200

    initial commit

---
+++ jobSched/Readme.md
@@ -0,0 +1,6 @@
+
+# 1235. Maximum Profit in Job Scheduling
+
+n jobs, each  with startTime integer values, endTime, and profit
+
+find best-profit non-overlapping job set
---
+++ jobSched/main.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <memory>
+#include <cassert>
+
+using namespace std;
+
+class Solution {
+public:
+int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+    map<int, int> times;
+    unordered_map<int, vector<pair<int, int>>> jobs;
+    for (auto i = 0; i < startTime.size(); ++i) {
+        times[startTime[i]] = 0;
+        jobs[startTime[i]].push_back({ endTime[i], profit[i] });
+    }    
+    int max_profit = 0;
+    for (auto it = rbegin(times); it != rend(times); ++it) {
+        for (auto job : jobs[it->first]) {
+            auto tmit = times.lower_bound(job.first);
+            max_profit = max(max_profit, (tmit == end(times) ? 0 : tmit->second) + job.second);
+        }
+        it->second = max_profit;
+    }
+    return max_profit;
+} 
+};
+
+int main()
+{
+
+    vector<int> startTime {1,2,3,3};
+    vector<int> endTime {3,4,5,6};
+    vector<int> profit {50,10,40,70};
+    int result = 120;
+
+    Solution solver;
+    assert(solver.jobScheduling(startTime,endTime,profit)==result);
+    
+    return 0;
+}
