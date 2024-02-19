5a786b53958ae19fd6fc2ea9c583f590cc43eb03
Razvan Coca <razvan.coca@capgemini.com>
2024-02-19 19:06:00 +0200

    changed from asserts to gtests

--- groupAnagrams/main.cpp
+++ groupAnagrams/main.cpp
@@ -2,8 +2,9 @@
 #include <string>
 #include <unordered_map>
 #include <set>
+#include <memory>
 #include <cassert>
-
+#include "gtest/gtest.h"
 
 using namespace std;
 class Solution {
@@ -22,23 +23,30 @@ public:
     }
 };
 
-int main()
+class MyFixture : public testing::Test {
+    static std::shared_ptr<Solution> pGrouper;
+ public:
+    static void SetUpTestSuite() { pGrouper=std::make_shared<Solution>(); }
+    static void TearDownTestSuite() { pGrouper.reset();}
+    static std::shared_ptr<Solution> getGrouper(){return pGrouper;}
+};
+std::shared_ptr<Solution> MyFixture::pGrouper;
+
+TEST(MyFixture,test1)
 {
-    Solution grouper;
-    {
-        vector<string> strs {"eat","tea","tan","ate","nat","bat"};
-        set<set<string>> anagrams{{"bat"},{"tan","nat"},{"eat","tea","ate"}};
-        assert(grouper.groupAnagrams(strs)==anagrams);
-    }
-    {
-        vector<string> strs{""};
-        set<set<string>> anagrams{{""}};
-        assert(grouper.groupAnagrams(strs)==anagrams);
-    }
-    {
-        vector<string> strs{"a"};
-        set<set<string>> anagrams{{"a"}};
-        assert(grouper.groupAnagrams(strs)==anagrams);
-    }
-    return 0;
+    vector<string> strs {"eat","tea","tan","ate","nat","bat"};
+    set<set<string>> anagrams{{"bat"},{"tan","nat"},{"eat","tea","ate"}};
+    EXPECT_EQ(MyFixture::getGrouper()->groupAnagrams(strs),anagrams);
+}
+TEST(MyFixture,test2)
+{
+    vector<string> strs{""};
+    set<set<string>> anagrams{{""}};
+    EXPECT_EQ(MyFixture::getGrouper()->groupAnagrams(strs),anagrams);
+}
+TEST(MyFixture,test3)
+{
+    vector<string> strs{"a"};
+    set<set<string>> anagrams{{"a"}};
+    EXPECT_EQ(MyFixture::getGrouper()->groupAnagrams(strs),anagrams);
 }
